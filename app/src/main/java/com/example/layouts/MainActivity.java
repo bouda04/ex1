06f2fbdcaadad244aabf69ff07f26d492c20df81
bouda04 <bouda04@gmail.com>
2016-09-16 23:13:06 +0300

    mainactivity

--- app/src/main/java/com/example/layouts/MainActivity.java
+++ app/src/main/java/com/example/layouts/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 
-		if (true);
+		if (false);
 		super.onSaveInstanceState(outState);
 	}
 
